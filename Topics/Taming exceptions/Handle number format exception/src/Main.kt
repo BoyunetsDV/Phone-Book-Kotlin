ddab9f37af346d5a9dff464a99feb21cf84fbec5
Denys Boiunets <boyunets.dv@gmail.com>
2021-05-04 00:32:10 +0300

    some more theory repeated

---
+++ Topics/Taming exceptions/Handle number format exception/src/Main.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun parseCardNumber(cardNumber: String): Long {
+    val numberParts = cardNumber.split(" ")
+    if (numberParts.size != 4 && !numberParts.all { it.length == 4 }) {
+        throw Exception("Invalid card number")
+    }
+    return numberParts.joinToString("").toLong()
+}
---
+++ Topics/What is an exception/Cause an exception/src/Main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val a = { 0 }
+    val b = 3
+    println(b / a())
+}
